828c3b34757e72cf00770442d2187fede15c447d
tshepoandi <lovekilledtshepo@gmail.com>
2021-06-24 20:23:05 +0200

    used array.join to turn array to string

---
+++ task0-10.js
@@ -0,0 +1,84 @@
+function commonChar()
+{
+
+  var string1 = prompt('whats your first string')
+  var string2 = prompt('whats your other string')
+  var array = []
+    
+  for (i = 0; i < string1.length; i++)
+
+  
+  {
+    if (string1.charAt(i) == string2.charAt(0))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+
+    if (string1.charAt(i) == string2.charAt(1))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+    
+    if (string1.charAt(i) == string2.charAt(2))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+
+    if (string1.charAt(i) == string2.charAt(3))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+    if (string1.charAt(i) == string2.charAt(4))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+
+    if (string1.charAt(i) == string2.charAt(5))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+    
+    if (string1.charAt(i) == string2.charAt(6))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+
+    if (string1.charAt(i) == string2.charAt(7))
+    { 
+      array.push(string1.charAt(i))
+    } 
+
+if (string1.charAt(i) == string2.charAt(8))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+    
+    if (string1.charAt(i) == string2.charAt(9))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+
+    if (string1.charAt(i) == string2.charAt(10))
+    { 
+      array.push(string1.charAt(i))
+    }
+
+
+
+    
+    }
+  var word = array.join(' ')
+  console.log(word)
+  
+} 
+  commonChar();
+

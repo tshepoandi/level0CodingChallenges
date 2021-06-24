83683f6c64228bc20938e85e6d5c1ac9e148748d
tshepoandi <lovekilledtshepo@gmail.com>
2021-06-24 20:26:48 +0200

    was told to.put a comma on this one

--- task0-8.js
+++ task0-8.js
@@ -1,6 +1,6 @@
 function minutesToHours()
 {
-  var yourMinutes = prompt("enter your minutes")
+  var yourMinutes = prompt('Please enter minutes')
   var minutesExact = yourMinutes % 60
   var hours = yourMinutes / 60
   var hoursExact = Math.floor(hours)
@@ -8,10 +8,10 @@ function minutesToHours()
   
   if (hoursExact > 1) 
   {
-  console.log(hoursExact + "hours " +minutesExact + "minutes")
+  console.log(hoursExact + "hours , " +minutesExact + "minutes")
   } else if (hours = 1) 
   {
-    console.log(hoursExact + "hour " +minutesExact + "minutes")
+    console.log(hoursExact + "hour , " +minutesExact + "minutes")
   } 
   
 

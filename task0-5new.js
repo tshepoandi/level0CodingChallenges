6a9c738b1d8d47e341e6f5361818ea19163cc381
tshepoandi <lovekilledtshepo@gmail.com>
2021-06-24 20:36:37 +0200

    was told not to hard code digits for sides

--- task0-5new.js
+++ task0-5new.js
@@ -1,12 +1,13 @@
 function triangleArea() {
 
-	var sideA = 3
-	var sideB = 4
-	var sideC = 5
+var sideA = prompt("enter digit for side A")
+var sideB = prompt("enter digit for side B")
+var sideC = prompt("enter digit for side C")
 
-var perimeter = parseInt(SideA) + parseInt(sideB) + parseInt(sideC)
+var perimeter = parseInt(sideA) + parseInt(sideB) + parseInt(sideC)
 
-var semiperimeter = perimeter / 2
+var semiperimeter = perimeter / 2 
+
 
 var semi1 = parseInt(sideA) - semiperimeter
 var semi2 = parseInt(sideB) - semiperimeter
@@ -15,13 +16,10 @@ var semi3 = parseInt(sideC) - semiperimeter
 var multiplySemiSides = semi1 * semi2 * semi3
 
 var area = multiplySemiSides * semiperimeter
-var absolute = Math.sqrt(area)
+var absolute = Math.abs(area);
 var squaredAbsolute = Math.sqrt(absolute)
 
 console.log(squaredAbsolute)
-
-
 }
-
 
 triangleArea();

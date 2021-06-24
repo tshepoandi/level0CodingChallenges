f78230a1b9c5fed28515454127db79dd32a4bbdd
tshepoandi <lovekilledtshepo@gmail.com>
2021-06-24 20:50:25 +0200

    was told not to hardcode numbers

--- task0-6new.js
+++ task0-6new.js
@@ -1,39 +1,38 @@
-function highestNumber() {
-
-	var num1 = prompt("enter your first number")
-	var num2 = prompt("enter your second number")
-	var num3 = prompt("enter your third number")
-
-	if (num1 > num2 && num1 > num3) 
-	{
-
-		return num1
-
-	}
-
-	else if (num2 > num1 && num2 > num3) 
-	{
-
-		return num2
-
-	}
-
-	else if (num3 > num1 && num3 > num2) 
-	{
-
-		return num3
-
-	}
-
-	else 
-	{
-
-		return 'you had two equally great numbers' 
-
-	}
-
+function highestNumber(){
+  var num1 = prompt('whats your first digit')
+  var num2 = prompt('whats your second digit')
+  var num3 = prompt('whats your third digit')
+  var equalNumber = 0
+  
+  if (num1 > num2 && num1 > num3) {
+    return num1
+  } else if (num2 > num1 && num2 > num3) {
+    return num2
+  } else if (num3 > num1 && num3 > num2) {
+    return num3
+  } else if (num2 == num3) 
+  {
+   var equalNumber = equalNumber + num3
+   console.log("some numbers were equal which were " + equalNumber)
+  } 
+  
+  else if (num1 == num2)
+  {
+    var equalNumber = equalNumber + num1
+    
+return "some numbers were equal which were " + equalNumber
+    
+    
+   } else if (num1 == num3)
+  {
+    var equalNumber = equalNumber + num3
+    
+return "some numbers were equal which were " + equalNumber
+  }else
+  
+  {
+    return 'did you type a key instead of numbers?')
+  }
 }
 
 highestNumber();
-
-

3845aa2ea4ab992f67d89af518de50e24cdf52ef
tshepoandi <lovekilledtshepo@gmail.com>
2021-06-28 18:18:57 +0200

    modified task8

--- task0-8.js
+++ task0-8.js
@@ -1,19 +1,20 @@
-function minutesToHours()
+
+function minutesToHours(yourMinutes)
 {
-  var yourMinutes = prompt('Please enter minutes')
+ 
   var minutesExact = yourMinutes % 60
   var hours = yourMinutes / 60
   var hoursExact = Math.floor(hours)
-  
-  
-  if (hoursExact > 1) 
+
+
+  if (hoursExact > 1 || hoursExact == 0)
   {
   console.log(hoursExact + "hours , " +minutesExact + "minutes")
-  } else if (hours = 1) 
+  } else if (hours = 1)
   {
     console.log(hoursExact + "hour , " +minutesExact + "minutes")
-  } 
-  
+  }
+
 
 }
-minutesToHours();
+minutesToHours(71);

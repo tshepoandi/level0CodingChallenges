44257c50bce9a8d5f3b2ff8b5abfa0e4cfe18f18
tshepo mashiloane <lovekilledtshepo@gmail.com>
2021-07-14 23:53:35 +0100

    made changees to task8

--- task0-8.js
+++ task0-8.js
@@ -1,18 +1,22 @@
-
 function minutesToHours(yourMinutes)
 {
- 
+
   var minutesExact = yourMinutes % 60
   var hours = yourMinutes / 60
   var hoursExact = Math.floor(hours)
-
-
+  
+  if (minutesExact > 1) {
+    var minutesPlural = " minutes"
+  }
+  else {
+    var minutesPlural = " minute"
+  }
   if (hoursExact > 1 || hoursExact == 0)
   {
-  console.log(hoursExact + "hours , " +minutesExact + "minutes")
+  console.log(hoursExact + "hours, " +minutesExact + minutesPlural)
   } else if (hours = 1)
   {
-    console.log(hoursExact + "hour , " +minutesExact + "minutes")
+    console.log(hoursExact + "hour, " +minutesExact + minutesPlural)
   }
 
 

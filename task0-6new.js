049cb5fa870c77c4b8b179b7bf4f4a41f961b8ff
tshepo mashiloane <lovekilledtshepo@gmail.com>
2021-07-14 23:46:14 +0100

    made changes to task6

--- task0-6new.js
+++ task0-6new.js
@@ -1,36 +1,36 @@
 function highestNumber(num1,num2,num3)
 {
   var equalNumber = 0
-  
+
   if (num1 > num2 && num1 > num3) {
     return num1
   } else if (num2 > num1 && num2 > num3) {
     return num2
   } else if (num3 > num1 && num3 > num2) {
     return num3
-  } else if (num2 == num3) 
+  } else if (num2 == num3)
   {
    var equalNumber = equalNumber + num3
-   console.log("some numbers were equal which were " + equalNumber)
-  } 
-  
+   return "some numbers were equal which were " + equalNumber
+  }
+
   else if (num1 == num2)
   {
     var equalNumber = equalNumber + num1
-    
+
 return "some numbers were equal which were " + equalNumber
-    
-    
+
+
    } else if (num1 == num3)
   {
     var equalNumber = equalNumber + num3
-    
+
 return "some numbers were equal which were " + equalNumber
   }else
-  
+
   {
-    return 'did you type a key instead of numbers?')
+    return 'did you type a key instead of numbers?'
   }
 }
 
-highestNumber();
+highestNumber(2,1,4);

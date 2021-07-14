3a3e1981c88d45998b5a3bb5c548d9ea031210bc
tshepo mashiloane <lovekilledtshepo@gmail.com>
2021-07-15 00:04:34 +0100

    made changes to task 9

--- task0-9 (1).js
+++ task0-9 (1).js
@@ -7,67 +7,66 @@ countforO = 0
 countforU = 0
 var array = [""]
 
-for (i = 0; i < string.length ; i++) 
+for (i = 0; i < string.length ; i++)
 {
-  if (string.charAt(i) == "a" || string.charAt(i) == "A") 
+  if (string.charAt(i) == "a" || string.charAt(i) == "A")
   {
     countforA = countforA + 1
-    if (countforA == 1) 
+    if (countforA == 1)
     {
     array.push(string.charAt(i))
-    array.push(",")
+    array.push("," + " ")
     }
-   
+
   }
-  
-  
-  if (string.charAt(i) == "e" || string.charAt(i) == "E") 
+
+
+  if (string.charAt(i) == "e" || string.charAt(i) == "E")
   {
     countforE= countforE + 1
-    if (countforE == 1) 
+    if (countforE == 1)
     {
     array.push(string.charAt(i))
-    array.push(",")
+    array.push("," + " ")
     }
-    
+
   }
-    
-  if (string.charAt(i) == "i" || string.charAt(i) == "I") 
+
+  if (string.charAt(i) == "i" || string.charAt(i) == "I")
   {
     countforI = countforI+ 1
-    if (countforI == 1) 
-    {
-    array.push(string.charAt(i))
-    array.push(",")
-    }
-    
-  }
-  
-  if (string.charAt(i) == "o" || string.charAt(i) == "O") 
-  {
-    countforO = countforO + 1
-    if (countforO == 1) 
-    {
-    array.push(string.charAt(i))
-    array.push(",")
-    }
-    
-  }
-    
-  if (string.charAt(i) == "u" || string.charAt(i) == "U") 
-  {
-    countforU = countforU + 1
-    if (countforU == 1) 
-    {
-    array.push(string.charAt(i))
-    array.push(",")
-    }
-  }
+    if (countforI == 1)
+ {
+ array.push(string.charAt(i))
+ array.push("," + " ")
+ }
+
 }
 
-array.pop(",")  
-console.log(array.join(" ".toLowerCase()))
+if (string.charAt(i) == "o" || string.charAt(i) == "O")
+{
+ countforO = countforO + 1
+ if (countforO == 1)
+ {
+ array.push(string.charAt(i))
+ array.push("," + " ")
+ }
+
+}
+
+if (string.charAt(i) == "u" || string.charAt(i) == "U")
+{
+ countforU = countforU + 1
+ if (countforU == 1)
+ {
+ array.push(string.charAt(i))
+ array.push("," + " ")
+ }
+}
+}
+
+array.pop(",")
+console.log("Vowels; " + array.join("").toLowerCase())
 }
 
 returnVowels('Umuzi testing vowels');
-

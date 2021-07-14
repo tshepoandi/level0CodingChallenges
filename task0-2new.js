e5bca06ae468f3def9595117b06fd0887eff79c8
tshepo mashiloane <lovekilledtshepo@gmail.com>
2021-07-14 23:23:31 +0100

    made changes to task 2

--- task0-2new.js
+++ task0-2new.js
@@ -1,17 +1,12 @@
-var x = 1 * 2
-var x = x + 1
-var y =( 1 + 1)
-var y = y * 2
-var z = (1 * 2)
-var z = z + 1
-var a = 2 / 2
-var a = a + 1
-var a = a + 1
-var b = (1 * 2)
-var b = (b + 1)
-var b = (b) / 2
-console.log(x)
-console.log(y)
-console.log(z)
-console.log(a)
-console.log(b)
+
+var x = 1 + 1 * 2
+var y = (1 + 1) * 2
+var z = 1 + (1 * 2)
+var a = 1 + 1 * 2 / 2
+var b = (1 + 1 * 2) / 2
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
+

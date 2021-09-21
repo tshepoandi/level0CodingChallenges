383858d039076470fe139cee5c6cc77a9b2d7980
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-09-21 04:44:42 +0200

    used prettier to format code

--- task0-8.js
+++ task0-8.js
@@ -1,24 +1,19 @@
-function minutesToHours(yourMinutes)
-{
+/** @format */
 
-  var minutesExact = yourMinutes % 60
-  var hours = yourMinutes / 60
-  var hoursExact = Math.floor(hours)
-  
-  if (minutesExact > 1 || minutesExact == 0 ) {
-    var minutesPlural = " minutes"
-  }
-  else {
-    var minutesPlural = " minute"
-  }
-  if (hoursExact > 1 || hoursExact == 0)
-  {
-  console.log(hoursExact + "hours, " +minutesExact + minutesPlural)
-  } else if (hours = 1)
-  {
-    console.log(hoursExact + "hour, " +minutesExact + minutesPlural)
-  }
+function minutesToHours(yourMinutes) {
+	var minutesExact = yourMinutes % 60;
+	var hours = yourMinutes / 60;
+	var hoursExact = Math.floor(hours);
 
-
+	if (minutesExact > 1 || minutesExact == 0) {
+		var minutesPlural = ' minutes';
+	} else {
+		var minutesPlural = ' minute';
+	}
+	if (hoursExact > 1 || hoursExact == 0) {
+		console.log(hoursExact + 'hours, ' + minutesExact + minutesPlural);
+	} else if ((hours = 1)) {
+		console.log(hoursExact + 'hour, ' + minutesExact + minutesPlural);
+	}
 }
 minutesToHours(61);

26974717fe2b79bf9ebcb784256e5c3c6e68f845
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-09-29 10:15:02 +0200

    refactored

--- task0-7new.js
+++ task0-7new.js
@@ -1,18 +1,17 @@
 function celciusToFarenheits(celcius) {
-
-	farenheitsStepOne = 9 / 5
-	totalFarenheits = farenheitsStepOne * parseFloat(celcius) + 32
-	return totalFarenheits + " farenheits" }
+	let farenheitsStepOne = 9 / 5;
+	let totalFarenheits = farenheitsStepOne * parseFloat(celcius) + 32;
+	return totalFarenheits + ' farenheits';
+}
 
 celciusToFarenheits(100);
 
 function farenheitsToCelcius(farenheits) {
+	let celciusCalculation = farenheits - 32;
+	let celciusCalculationStepTwo = 5 / 9;
+	let totalCelcius = parseFloat(celciusCalculation) * celciusCalculationStepTwo;
 
-	var celciusCalculation = farenheits - 32
-	var celciusCalculationStepTwo = 5 / 9 
-	var totalCelcius = parseFloat(celciusCalculation) * celciusCalculationStepTwo
-
-	return totalCelcius + " celcius"
+	return totalCelcius + ' celcius';
 }
 
 farenheitsToCelcius(100);

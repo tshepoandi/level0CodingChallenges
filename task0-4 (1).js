f0490815db9857ea9717f44d8e1df5b02712039e
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-09-29 10:08:32 +0200

    refactored

--- task0-4 (1).js
+++ task0-4 (1).js
@@ -1,12 +1,9 @@
-function evenOrOdd(interger){
-  
-  var userNumber = parseInt(interger)
-  if (userNumber % 2 === 0) {
-    console.log('even')
-  }
-  else {
-    console.log('odd')
-  }
+function evenOrOdd(interger) {
+	let userNumber = parseInt(interger);
+	if (userNumber % 2 === 0) {
+		console.log('even');
+	} else {
+		console.log('odd');
+	}
 }
-
 evenOrOdd(33);

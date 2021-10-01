de9c9359f5c043d7ef837b5e78b8388b4492a42f
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-10-01 13:34:39 +0200

    refactored

--- task0-9 (1).js
+++ task0-9 (1).js
@@ -1,58 +1,16 @@
-
-function returnVowels(string) {
-	let countforA = 0;
-	let countforE = 0;
-	let countforI = 0;
-	let countforO = 0;
-	let countforU = 0;
-	let array = [''];
-
-	for (i = 0; i < string.length; i++) {
-		if (string.charAt(i) == 'a' || string.charAt(i) == 'A') {
-			countforA = countforA + 1;
-			if (countforA == 1) {
-				array.push(string.charAt(i));
-				array.push(',' + ' ');
-			}
-		}
-
-		if (string.charAt(i) == 'e' || string.charAt(i) == 'E') {
-			countforE = countforE + 1;
-			if (countforE == 1) {
-				array.push(string.charAt(i));
-				array.push(',' + ' ');
-			}
-		}
-
-		if (string.charAt(i) == 'i' || string.charAt(i) == 'I') {
-			countforI = countforI + 1;
-			if (countforI == 1) {
-				array.push(string.charAt(i));
-				array.push(',' + ' ');
-			}
-		}
-
-		if (string.charAt(i) == 'o' || string.charAt(i) == 'O') {
-			countforO = countforO + 1;
-			if (countforO == 1) {
-				array.push(string.charAt(i));
-				array.push(',' + ' ');
-			}
-		}
-
-		if (string.charAt(i) == 'u' || string.charAt(i) == 'U') {
-			countforU = countforU + 1;
-			if (countforU == 1) {
-				array.push(string.charAt(i));
-				array.push(',' + ' ');
+function getVowels(string) {
+	arrayOfVowels = ['a', 'e', 'i', 'o', 'u'];
+	arrayOfOutput = [];
+	for (let i = 0; i < string.length; i++) {
+		if (arrayOfVowels.includes(string.charAt(i))) {
+			if (arrayOfOutput.includes(string.charAt(i))) {
+			} else {
+				arrayOfOutput.push(string.charAt(i));
 			}
 		}
 	}
-
-	array.pop(',');
-	console.log('Vowels: ' + array.join('').toLowerCase());
+	return `Vowels: ${arrayOfOutput.join(', ')}`;
 }
-
-returnVowels('Umuzi iii ');
+console.log(getVowels('Umuzi i i i u u '));
 
 

394f7b5fe9400daebb6f1c7f9199c18cf1eaa3bf
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-10-01 10:24:21 +0200

    refactored

--- task0-5new.js
+++ task0-5new.js
@@ -1,19 +1,19 @@
 function triangleArea(sideA, sideB, sideC) {
-	let perimeter = parseInt(sideA) + parseInt(sideB) + parseInt(sideC);
+	const perimeter = parseInt(sideA) + parseInt(sideB) + parseInt(sideC);
 
-	let semiperimeter = perimeter / 2;
+	const semiPerimeter = perimeter / 2;
 
-	let semi1 = parseInt(sideA) - semiperimeter;
-	let semi2 = parseInt(sideB) - semiperimeter;
-	let semi3 = parseInt(sideC) - semiperimeter;
+	let semi1 = parseInt(sideA) - semiPerimeter;
+	let semi2 = parseInt(sideB) - semiPerimeter;
+	let semi3 = parseInt(sideC) - semiPerimeter;
 
-	let multiplySemiSides = semi1 * semi2 * semi3;
+	let semiPerimetersMultiplied = semi1 * semi2 * semi3;
 
-	let area = multiplySemiSides * semiperimeter;
+	let area = semiPerimetersMultiplied * semiPerimeter;
 	let absolute = Math.abs(area);
 	let squaredAbsolute = Math.sqrt(parseInt(absolute));
 
 	return squaredAbsolute;
 }
-console.log(triangleArea(2, 2, 2));
+triangleArea(2, 2, 2);
 

3c6a660f03f8462ba7fb6926e8477645dbbd3afe
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-10-01 10:54:46 +0200

    refactored

--- task0-6new.js
+++ task0-6new.js
@@ -1,25 +1,12 @@
-function highestNumber(num1, num2, num3) {
-	if (num1 > num2 && num1 > num3) {
-		return num1;
-	} else if (num2 > num1 && num2 > num3) {
-		return num2;
-	} else if (num3 > num1 && num3 > num2) {
-		return num3;
-	} else if (num2 == num3) {
-		let equalNumber = equalNumber + num3;
-		return 'some numbers were equal which were ' + equalNumber;
-	} else if (num1 == num2) {
-		let equalNumber = equalNumber + num1;
-
-		return 'some numbers were equal which were ' + equalNumber;
-	} else if (num1 == num3) {
-		let equalNumber = equalNumber + num3;
-
-		return 'some numbers were equal which were ' + equalNumber;
-	} else {
-		return 'did you type a key instead of numbers?';
+function highestNumber() {
+	let standByNumber = 0;
+	for (let i = 0; i < arguments.length; i++) {
+		if (arguments[i] > standByNumber) {
+			standByNumber = arguments[i];
+		}
 	}
+	console.log(standByNumber);
 }
 
-highestNumber(2, 1, 4);
+highestNumber(2, 1, 6, 4);
 

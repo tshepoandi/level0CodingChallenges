590eba46d1cbf41381b87c5f3e1c20399bbade76
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-10-06 12:28:07 +0200

    fixed bug

--- task0-6new.js
+++ task0-6new.js
@@ -1,12 +1,10 @@
 function highestNumber() {
-	let standByNumber = 0;
+	let standByNumber = -Infinity;
 	for (let i = 0; i < arguments.length; i++) {
 		if (arguments[i] > standByNumber) {
 			standByNumber = arguments[i];
 		}
 	}
-	return standByNumber;
+	console.log(standByNumber);
 }
-
-highestNumber(2, 1, 6, 4);
-
+highestNumber(-2, -1, -3, -4);

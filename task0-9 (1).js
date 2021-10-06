9bc5130f618e2707cecc2052ba1efae1f44d8c08
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-10-06 17:15:51 +0200

    refactored

--- task0-9 (1).js
+++ task0-9 (1).js
@@ -2,13 +2,13 @@ function getVowels(string) {
 	let arrayOfVowels = ['a', 'e', 'i', 'o', 'u'];
 	let arrayOfOutput = [];
 	for (let i = 0; i < string.length; i++) {
-		if (arrayOfVowels.includes(string.charAt(i))) {
-			if (arrayOfOutput.includes(string.charAt(i)) == false) {
-				arrayOfOutput.push(string.charAt(i));
+		if (arrayOfVowels.includes(string.charAt(i).toLowerCase())) {
+			if (arrayOfOutput.includes(string.charAt(i)) === false) {
+				arrayOfOutput.push(string.charAt(i).toLowerCase());
 			}
 		}
 	}
 	console.log(`Vowels: ${arrayOfOutput.join(', ')}`);
 }
-getVowels('Umuzi i i i u u ');
+getVowels('UmuzI uu ii');
 

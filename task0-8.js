fcb049ee53bba39f03a942438f4939f3e580a6b6
tshepoandi <85301167+tshepoandi@users.noreply.github.com>
2021-10-07 08:49:55 +0200

    refactored

--- task0-8.js
+++ task0-8.js
@@ -1,21 +1,22 @@
+
 function minutesToHours(yourMinutes) {
 	let minutesExact = yourMinutes % 60;
 	let hours = yourMinutes / 60;
 	let hoursExact = Math.floor(hours);
 	let minutesPlural = '';
-	if (minutesExact > 1 || minutesExact == 0) {
+	if (minutesExact > 1 || minutesExact === 0) {
 		minutesPlural = 'minutes';
 	} else {
 		minutesPlural = 'minute';
 	}
-	if (hoursExact > 1 && hoursExact != 0) {
+	if (hoursExact > 1 || hoursExact === 0) {
 		console.log(`${hoursExact} hours, ${minutesExact} ${minutesPlural}`);
-	} else if (hoursExact == 1 && hoursExact != 0) {
+	} else if (hoursExact === 1) {
 		console.log(`${hoursExact} hour, ${minutesExact} ${minutesPlural}`);
-	} else if (hoursExact == 0) {
+	} else if (hoursExact === 0) {
 		console.log(`${minutesExact} ${minutesPlural}`);
 	}
 }
-minutesToHours(22);
+minutesToHours(00);
 minutesToHours(71);
 minutesToHours(133);
